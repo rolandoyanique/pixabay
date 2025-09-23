f94b20e21327548ebf2daa9772fe5fbea50cc660
Rolando <rolyan66@gmail.com>
2025-09-22 21:49:23 -0400

    Se realizo la adicion del spinner y los botones para la paginaciòn

--- src/app/components/listar-imagen/listar-imagen.component.ts
+++ src/app/components/listar-imagen/listar-imagen.component.ts
@@ -11,6 +11,7 @@ export class ListarImagenComponent implements OnInit {
   termino='';
   suscripcion:Subscription;
   listImagenes:any[]=[];
+  loading:boolean=false;
   constructor(private _imagenService:ImagenService) { 
     this.suscripcion=this._imagenService.getTerminoBusqueda().subscribe(data=>{
       this.termino=data;
@@ -21,12 +22,17 @@ export class ListarImagenComponent implements OnInit {
   ngOnInit(): void {
   }
   obtenerImagenes(){
+    this.loading=true;
     this._imagenService.getImagenes(this.termino).subscribe(data=>{
       if(data.hits.length===0){
         this._imagenService.setError('Obs... no hay resultados');
         return;
       }
-      this.listImagenes=data.hits;
+      setTimeout(()=>{
+        this.loading=false;
+        this.listImagenes=data.hits;
+      },2000);
+      
       console.log(this.listImagenes);
     })
   }
